7a9e13a45792885e6e11106e4b48c7888258296c
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-12-02 17:43:50 -0500

    All responses besides ACMD41 are correct, need to debug further

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,10 +1,50 @@
 // Used as reference for SD card commands https://chlazza.nfshost.com/sdcardinfo.html
 
 #include "main.h"
+#include <stdio.h>
 
 void SPI_Init();
+void USART_Init();
+uint8_t SD_Init();
+void SD_PowerUp();
+uint8_t SD_SendCMD0();
+uint8_t SD_SendCMD8();
+uint8_t SD_SendCMD58();
+uint8_t SD_SendCMD55();
+uint8_t SD_SendACMD41();
+uint8_t SD_Card_Init();
+uint8_t SD_Init();
+
 
 int main() {
+	SPI_Init();
+	USART_Init();
+	printf("READY\r\n");
+
+	uint8_t response;
+
+	// Test power up sequence
+	SD_PowerUp();
+	printf("Power up sequence complete\r\n");
+
+    // Test CMD0
+	response = SD_SendCMD0();
+	printf("CMD0 Response: 0x%02X\r\n", response);  // Should be 0x01
+
+	// Test CMD8
+	response = SD_SendCMD8();
+	printf("CMD8 Response: 0x%02X\r\n", response);  // Should be 0x01
+
+	response = SD_SendCMD58();
+	printf("CMD58 Response: 0x%02X\r\n", response);
+
+	// Test CMD55 (by itself first)
+	response = SD_Card_Init();
+	printf("CMD55/ACMD41 Response: 0x%02X\r\n", response);  // Should be 0x00
+
+	while (1) {
+
+	}
 
 }
 
@@ -24,7 +64,7 @@ void SPI_Init() {
 
 	SPI1 -> CR1 = 0;																	// Resets SPI1
 	SPI1 -> CR1 |= (1 << 2);															// Sets Master mode
-	SPI1 -> CR1 |= (5 << 3);															// /64 for initialization of SD card
+	SPI1 -> CR1 |= (7 << 3);															// /256 for initialization of SD card
 	SPI1 -> CR1 |= (1 << 8);															// Used to select any pin for NSS instead of hardware
 	SPI1 -> CR1 |= (1 << 9);															// Enables SSM
 	SPI1 -> CR1 |= (1 << 6);															// SPI1 Enable
@@ -61,16 +101,16 @@ uint8_t SD_SendCMD0() {
 	SD_Select();																		// Selects SD card
 
 	SPI_Transfer(0x40);																	// CMD0 begin
-	SPI_Transfer(0x00);
-	SPI_Transfer(0x00);
-	SPI_Transfer(0x00);
-	SPI_Transfer(0x00);
-	SPI_Transfer(0x95);																	// CRC required for CMD0
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x95);																	// CRC
 
 	uint8_t response;
 
-	// Gives the SD card 80 cycles to respond
-	for (int i = 0; i < 10; i++) {
+
+	for (int i = 0; i < 10; i++) {														// Gives the SD card 80 cycles to respond
 		response = SPI_Transfer(0xFF);
 		if (response != 0xFF) break;
 	}
@@ -81,16 +121,46 @@ uint8_t SD_SendCMD0() {
 	return response;																	// Should be 0x01 if successful
 }
 
+// Checks if SD card can operate at current device voltage; required for v2+
+// Implement error checking to make sure the check pattern and voltage is enough
 uint8_t SD_SendCMD8() {
+
+	SPI_Transfer(0xFF);
 
 	SD_Select();
 
-	SPI_Transfer(0x48);																	// CMD8 begin 72 + 0 + 0 + 1 + 170
-	SPI_Transfer(0x00);
-	SPI_Transfer(0x00);
-	SPI_Transfer(0x01);
-	SPI_Transfer(0xAA);
-	SPI_Transfer(0x87);
+	SPI_Transfer(0x48);																	// CMD8
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x01);																	// Voltage level 2.7-3.6V
+	SPI_Transfer(0xAA);																	// Check pattern
+	SPI_Transfer(0x87);																	// CRC
+
+	uint8_t response[5];																// R7 Format - Returns voltage info and check pattern
+
+	for (int i = 0; i < 5; i++) {
+		response[i] = SPI_Transfer(0xFF);
+	}
+
+	SD_Deselect();																		// Deselects SD card and runs a cycle
+	SPI_Transfer(0xFF);
+
+	return response[1];																	// Should be 0x01 if successful
+}
+
+// Returns a 5 byte response containing voltage and capacity information
+uint8_t SD_SendCMD58() {
+
+	SPI_Transfer(0xFF);
+
+	SD_Select();
+
+	SPI_Transfer(0x7A);    																// CMD58
+	SPI_Transfer(0x00);    																// Reserved
+	SPI_Transfer(0x00);    																// Reserved
+	SPI_Transfer(0x00);    																// Reserved
+	SPI_Transfer(0x00);    																// Reserved
+	SPI_Transfer(0xFF);    																// No CRC required
 
 	uint8_t response[5];
 
@@ -101,5 +171,119 @@ uint8_t SD_SendCMD8() {
 	SD_Deselect();
 	SPI_Transfer(0xFF);
 
-	return response[0];
+	return response[0];																	// Should be 0x01
+}
+
+// Tells card that next command is application specific
+// Implement error checking
+uint8_t SD_SendCMD55() {
+
+	SPI_Transfer(0xFF);
+
+	SD_Select();
+
+	SPI_Transfer(0x77);																	// CMD55
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0xFF);																	// No CRC required
+
+	uint8_t response;
+
+	for (int i = 0; i < 10; i++) {
+		response = SPI_Transfer(0xFF);
+		if (response != 0xFF) break;
+	}
+
+	SD_Deselect();
+	SPI_Transfer(0xFF);
+
+	return response;																	// Should be 0x01
+}
+
+// Activates SD card initialization process; must follow CMD55 for each attempt
+uint8_t SD_SendACMD41() {
+
+	SPI_Transfer(0xFF);
+
+	SD_Select();
+
+	SPI_Transfer(0x69);																	// ACMD41
+	SPI_Transfer(0x40);																	// HCS bit
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0x00);																	// Reserved
+	SPI_Transfer(0xFF);																	// No CRC required
+
+	uint8_t response;
+
+	for (int i = 0; i < 20; i++) {
+		response = SPI_Transfer(0xFF);
+		if (response != 0xFF) break;
+	}
+
+	SD_Deselect();
+	SPI_Transfer(0xFF);
+
+	return response;
+}
+
+// Initializes the SD card and keeps sending the commands until initialization completes
+uint8_t SD_Card_Init() {
+	uint8_t response;
+
+	do {
+		response = SD_SendCMD55();
+		if (response != 0x01) return response;
+
+		response = SD_SendACMD41();
+	} while (response == 0x01);
+
+	return response;
+}
+
+// Full SD card setup
+uint8_t SD_Init() {
+	uint8_t response;
+
+	SD_PowerUp();
+
+	response = SD_SendCMD0();
+	if(response != 0x01) return response;
+
+	response = SD_SendCMD8();
+	if (response != 0x01) return response;
+
+	response = SD_Card_Init();
+	if (response != 0x00) return response;
+
+	return 0;
+}
+
+void USART_Init() {
+	RCC -> APB1ENR |= (1 << 17);															// USART2 Clock
+	RCC -> AHB1ENR |= (1 << 0);																// GPIOA Clock; should be already enabled
+
+	GPIOA -> MODER &= ~((3 << (2 * 2)) | (3 << (2 * 3)));									// PA2 & PA3 to Alternate Function
+	GPIOA -> MODER |= (2 << (2 * 2)) | (2 << (2 * 3));
+
+	GPIOA -> AFR[0] |= (7 << (4 * 2)) | (7 << (4 * 3));										// Sets alternate function for USART2
+
+	USART2 -> BRR = 0x0683;																	// Baud rate set to 9600
+
+	USART2 -> CR1 |= (1 << 2) | (1 << 3);													// Receiver & Transmitter Enabled
+	USART2 -> CR1 |= (1 << 13);																// USART2 Enabled
+
+	printf("USART configured...........\r\n");
+}
+
+// printf() retargeting
+int __io_putchar(int c) {
+	while(!(USART2 -> SR & (1 << 7)));
+	USART2 -> DR = c;
+
+	while(!(USART2 -> SR & (1 << 6)));
+	USART2 -> SR &= ~(1 << 6);
+	return c;
 }
